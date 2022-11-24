5652c24d620de1b41e2ac42f862cac3930928ecb
vikas harge <harge.vikas@gmail.com>
2022-11-24 19:29:55 +0530

    BookingSchema Added

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -1,5 +1,58 @@
  const mongoose = require('mongoose')
 
- const orderSchema  = new mongoose.Schema({
+ const bookingModel  = new mongoose.Schema({
+
+    campSite : {
+        type : mongoose.Schema.ObjectId,
+        ref : "CampSite",
+        required : true,
+    },
+    user : {
+        type : mongoose.Schema.ObjectId,
+        ref : "User",
+        required : true,
+    },
+    numOfAdult : {
+        type : Number,
+        required : true,
+        default : 0,
+    },
+    numOfChildrens : {
+        type : Number,
+        required : true,
+        default : 0,
+    },
+    bookOn : {
+        type : Date,
+        default : Date.now,
+        required : true,
+    },
+    bookingDate : {
+        type : Date,
+        required : true,
+    },
+    packagePrice : {
+        type : Number,
+        required : true,
+        default : 0,
+    },
+    paymentInfo : {
+        id : {
+            type : String,
+            required : true,
+        },
+        status : {
+            type : String,
+            required : true,
+        }
+    },
+    bookingStatus : {
+        type : String,
+        required : true,
+        default : "Processing"
+    },
+    completedOn : Date,
      
  })
+
+ module.exports = mongoose.model("booking", bookingModel); 
